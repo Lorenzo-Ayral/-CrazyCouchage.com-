9e4478f0620ed6bb90e991346671e43c76741176
NicolasSanna <nico13sanna@gmail.com>
2023-04-26 12:03:51 +0200

    Sécuriser liens protégés

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -25,7 +25,7 @@ class RegistrationController extends AbstractController
         $this->emailVerifier = $emailVerifier;
     }
 
-    #[Route('/register', name: 'app_register')]
+    #[Route('/inscription', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
