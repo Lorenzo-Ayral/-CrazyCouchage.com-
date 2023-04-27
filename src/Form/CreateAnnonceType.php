4b61db085f7f378d52ca54706cdfdc65203e6b3b
NicolasSanna <nico13sanna@gmail.com>
2023-04-27 12:39:40 +0200

    AnnonceType

--- src/Form/CreateAnnonceType.php
+++ src/Form/CreateAnnonceType.php
@@ -33,7 +33,7 @@ class CreateAnnonceType extends AbstractType
                 'attr' => ['accept' => 'image/*'], // Filtre les fichiers pour n'accepter que les images
                 // Autres options supplémentaires selon vos besoins
             ])
-            ->add('description', TextareaType::class)
+            ->add('description', TextType::class)
             ->add('price', IntegerType::class, [
                 'label' => 'Prix'
             ])
